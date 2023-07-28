3bcaaa31a6153aa43aeebf0bcd4e6dceb7896f91
Katherine Cunniffe <katherinecunniffe@Katherines-MacBook-Air.local>
2023-07-28 17:06:17 +0100

    first commit

---
+++ test1.py
@@ -0,0 +1,13 @@
+print("hello")
+print("hi")
+print("hi")
+print("age?", end=' ')
+age = input()
+print("name?", end=' ')
+name = input()
+
+print(f"my name is {name}, I am {age}")
+
+age = input("age? ")
+name = input("name? ")
+print(f"my name is {name}, I am {age}")
